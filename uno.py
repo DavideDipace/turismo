a3cc26d655bd81e5201c3a93af8b7fbd8b161cd3
DavideDipace <dipace.davide@iisgalvanimi.edu.it>
2023-03-15 13:27:00 +0000

    terzo commit

--- uno.py
+++ uno.py
@@ -2,8 +2,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def Citta():
+def citta():
   return render_template("citta.html")
+
+@app.route('/Venezia')
+def Venezia():
+    return render_template("Venezia.html")
+
+@app.route('/Milano')
+def Milano():
+    return render_template("Milano.html")
+
+@app.route('/Firenze')
+def Firenze():
+    return render_template("Firenze.html")
+
+@app.route('/Napoli')
+def Napoli():
+    return render_template("Napoli.html")
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
